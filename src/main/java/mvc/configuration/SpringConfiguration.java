f0f0db23800faecae349a6b195a8f772e639414f
teatro <teatrocamillo@gmail.com>
2020-11-30 16:17:00 +0100

    Added configuration & service.

---
+++ src/main/java/mvc/configuration/SpringConfiguration.java
@@ -0,0 +1,13 @@
+package mvc.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfiguration {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
