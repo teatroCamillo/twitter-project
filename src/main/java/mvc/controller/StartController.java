fc94f04c5f7e801604cf0dd4a17060ff9bca61c7
teatro <teatrocamillo@gmail.com>
2020-12-10 19:05:36 +0100

    Style start page login and password field.

--- src/main/java/mvc/controller/StartController.java
+++ src/main/java/mvc/controller/StartController.java
@@ -2,21 +2,13 @@ package mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class StartController {
 
     @GetMapping("/start")
-    public String showStart2(){
+    public String showStart(){
         return "start";
     }
-
-//    @GetMapping("/start")
-//    public ModelAndView showStart(){
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("start");
-//        return modelAndView;
-//    }
 
 }
