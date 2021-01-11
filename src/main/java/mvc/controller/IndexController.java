1f0b63d3134f7fe076aabb8aaeda686efcb30ba2
teatro <teatrocamillo@gmail.com>
2021-01-11 20:08:15 +0100

    Added main template of userpage.html

--- src/main/java/mvc/controller/IndexController.java
+++ src/main/java/mvc/controller/IndexController.java
@@ -1,22 +1,14 @@
 package mvc.controller;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/userpage")
-    public ModelAndView test(){
-        Authentication authentication = SecurityContextHolder
-                .getContext()
-                .getAuthentication();
-        return new ModelAndView("userpage","login",
-                authentication.getName() + " " + authentication.getAuthorities());
+    @GetMapping("/")
+    public String showStart(){
+        return "index";
     }
-
 
 }
